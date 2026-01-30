0dc5d47996f4ae0197c9dea3b7088a8e3d0770d3
Nguyễn Hữu Giáp <giap13022005@gmail.com>
2026-01-30 16:38:45 +0700

    feat: add methob in brancontroller

--- src/main/java/com/nguyenhuugiap/computer_shop/controller/BrandController.java
+++ src/main/java/com/nguyenhuugiap/computer_shop/controller/BrandController.java
@@ -8,18 +8,20 @@ import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/brand")
+@RequestMapping("/brands")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class BrandController {
     BrandService brandService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<BrandResponse> createBrand(@RequestBody @Valid BrandRequest request) {
         return ApiResponse.<BrandResponse>builder()
                 .result(brandService.createBrand(request))
@@ -30,5 +32,25 @@ public class BrandController {
     public ApiResponse<List<BrandResponse>> getAllBrands() {
         return ApiResponse.<List<BrandResponse>>builder()
                 .result(brandService.getAllBrands()).build();
+    }
+
+    @GetMapping("/{id}")
+    public ApiResponse<BrandResponse> getBrandById(@PathVariable Long id) {
+        return ApiResponse.<BrandResponse>builder()
+                .result(brandService.getBrandById(id)).build();
+    }
+
+    @PatchMapping("/{id}")
+    public ApiResponse<BrandResponse> updateBrand(@PathVariable Long id, @RequestBody @Valid BrandRequest request) {
+        return ApiResponse.<BrandResponse>builder()
+                .result(brandService.updateBrand(id, request))
+                .build();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ApiResponse<Void> deleteBrand(@PathVariable Long id) {
+        brandService.deleteBrand(id);
+        return ApiResponse.<Void>builder().build();
     }
 }
