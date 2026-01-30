7d579943a368d89997fc97b27acc859b84036818
Nguyễn Hữu Giáp <giap13022005@gmail.com>
2026-01-30 17:10:39 +0700

    refactor(brand): refactor format dto

--- src/main/java/com/nguyenhuugiap/computer_shop/dto/request/BrandUpdateRequest.java
+++ src/main/java/com/nguyenhuugiap/computer_shop/dto/request/BrandUpdateRequest.java
@@ -1,6 +1,5 @@
 package com.nguyenhuugiap.computer_shop.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
