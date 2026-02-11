55a06d4ccb261018d600dea0aef4c8d099091e46
Nguyễn Hữu Giáp <giap13022005@gmail.com>
2026-02-11 23:53:04 +0700

    feat(exception): handling methob commece in JwtAuthenticationEntryPoint

--- src/main/java/com/nguyenhuugiap/computer_shop/configuration/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/nguyenhuugiap/computer_shop/configuration/JwtAuthenticationEntryPoint.java
@@ -1,8 +1,12 @@
 package com.nguyenhuugiap.computer_shop.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nguyenhuugiap.computer_shop.dto.response.ApiResponse;
+import com.nguyenhuugiap.computer_shop.exception.ErrorCode;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -12,6 +16,16 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+        response.setStatus(errorCode.getHttpStatusCode().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
+        ApiResponse<?> apiResponse = ApiResponse.builder()
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .build();
+        ObjectMapper mapper = new ObjectMapper();
+        response.getWriter().write(mapper.writeValueAsString(apiResponse));
+        response.flushBuffer();
     }
 }
